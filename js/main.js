58d0c537f9786e920997519f3dc27097c05a4b57
Daniel Rajendran <shadowise@gmail.com>
2019-02-23 11:34:43 +0000

    Added employment timeline
    Added roadmap.js and assosciated files to display my education and work history.

--- js/main.js
+++ js/main.js
@@ -48,3 +48,54 @@ $(document).ready(function() {
 		});
 	});
 });
+
+$.fn.isInViewport = function() {
+	var elementTop = $(this).offset().top;
+	var elementBottom = elementTop + $(this).outerHeight();
+
+	var viewportTop = $(window).scrollTop();
+	var viewportBottom = viewportTop + $(window).height();
+
+	return elementBottom > viewportTop && elementTop < viewportBottom;
+};
+
+var myEvents = [
+	{
+		date: '<img src="img/story-logo-goldsmiths.svg" id="" title="Goldsmiths, University of London logo" alt="Goldsmiths, University of London" />',
+		content: '<strong>BSc Interaction Design</strong></br><span class="font-small">The course focused on web design fundamentals, user experience and human interaction design.</span>'
+	},
+	{
+		date: '<img src="img/story-logo-huddle.svg" id="" title="Huddle logo" alt="Huddle" />',
+		content: '<strong>Design Intern</strong></br><span class="font-small">Assisted the marketing team with various web and print design tasks, as well as helping at events</span>'
+	},
+	{
+		date: '<img src="img/story-logo-huddle.svg" id="" title="Huddle logo" alt="Huddle" />',
+		content: '<strong>Junior Designer</strong></br><span class="font-small">Designed all sales and marketing focused content, including websites, emails, collateral, and merchandise</span>'
+	},
+	{
+		date: '<img src="img/story-logo-huddle.svg" id="" title="Huddle logo" alt="Huddle" />',
+		content: '<strong>Web Designer</strong></br><span class="font-small">Worked primarily on web and email, handling design, coding, QA, deployment, and platform integration</span>'
+	},
+	{
+		date: '<img src="img/story-logo-datasift.svg" id="" title="DataSift logo" alt="DataSift" />',
+		content: '<strong>Front-End Designer/Developer</strong></br><span class="font-small">Lead design and coding for marketing websites, and supported the product team with UX/UI work</span>'
+	},
+	{
+		date: '<img src="img/story-logo-content-code.svg" id="" title="Content and Code logo" alt="Content and Code" />',
+		content: '<strong>Product Designer</strong></br><span class="font-small">Lead design efforts for Fresh Intranet and Fresh Mobile, whilst helping the bids and marketing teams</span>'
+	},
+	{
+		date: '<img src="img/story-logo-next.svg" id="" title="The Next Adventure" alt="The Next Adventure" />',
+		content: '<strong>An Exciting New Role</strong></br><span class="font-small">I&#39;m currently looking for interesting new opportunities. If you&#39;re looking for a multi-talented designer, <a href="#contact" title="get in touch">let&#39;s talk</a>.</span>'
+	}
+];
+
+$(window).on('resize scroll', function() {
+	if ($('#journey').isInViewport()) {
+		$('#my-timeline').roadmap(myEvents,{
+			eventsPerSlide: 7,
+			orientation: 'auto'
+		});
+		$(window).off(' resize scroll');
+	}
+});
